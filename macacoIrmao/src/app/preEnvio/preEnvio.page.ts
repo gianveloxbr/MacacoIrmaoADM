55edc026d692acd44e9bb35415d811085fc40804
Gian Michel <gianveloxsi@gmail.com>
2019-05-13 10:28:19 -0300

    Modificando página de envio para conseguir camera e localização

--- macacoIrmao/src/app/preEnvio/preEnvio.page.ts
+++ macacoIrmao/src/app/preEnvio/preEnvio.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera,CameraOptions } from '@ionic-native/camera/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder,NativeGeocoderOptions,NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
 
 @Component({
   selector: 'app-preEnvio',
@@ -11,12 +14,68 @@ import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 
 export class PreEnvioPage implements OnInit{
-  constructor(private camera:Camera, private webview: WebView){
+
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+  endereco: string;
+
+  geoencoderOptions: NativeGeocoderOptions = {
+    useLocale: true,
+    maxResults: 5
+  }
+  constructor(private camera:Camera, private webview: WebView, private androidPermissions:AndroidPermissions,
+    private geoLocation: Geolocation, private nativeGeocoder: NativeGeocoder){
     
   }
 
   ngOnInit(){
-    this.tirarFoto();
+    this.checaPermissao();
+    this.getGeolocation();
+  }
+
+  //Endereco
+  getGeolocation(){
+    this.geoLocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude; 
+      this.accuracy = resp.coords.accuracy; 
+      this.getGeoencoder(this.latitude,this.longitude);
+     }).catch((error) => {
+       alert('Error getting location'+ JSON.stringify(error));
+     });
+  }
+
+  getGeoencoder(latitude,longitude){
+    this.nativeGeocoder.reverseGeocode(latitude, longitude, this.geoencoderOptions)
+    .then((result: NativeGeocoderResult[]) => {
+      this.endereco = this.generateEndereco(result[0]);
+    })
+    .catch((error: any) => {
+      alert('Erro ao localizar: '+ JSON.stringify(error));
+    });
+  }
+
+  generateEndereco(addressObj){
+      let obj = [];
+      let address = "";
+      for (let key in addressObj) {
+        obj.push(addressObj[key]);
+      }
+      obj.reverse();
+      for (let val in obj) {
+        if(obj[val].length)
+        address += obj[val]+', ';
+      }
+    return address.slice(0, -2);
+  }
+
+  //Checa se usuário possui permissão para usar câmera
+  checaPermissao(){    
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+      result => this.tirarFoto(),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
+    );    
   }
 
   tirarFoto(){
