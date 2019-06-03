2cfa715017220292228907b62e9a8c27eb815dcb
Gian Michel <gianveloxsi@gmail.com>
2019-06-03 15:18:03 -0300

    Função ainda não funcional

--- macacoIrmao/functions/index.js
+++ macacoIrmao/functions/index.js
@@ -1,9 +1,31 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.notificacao = functions.firestore
+    .document('ocorrencia/{ocorrenciaId}')
+    .onCreate((snap,context) => {
+
+        const data = snap.data();
+        const idAdmin = data.context.idAdmin
+        
+
+          const db = admin.firestore()
+      
+          // send a notification to each device token
+      
+          return Promise.all([admin.database().ref('admin').once('value')]).then(
+              results => {
+                  const token = results[1];
+                  if(!token.hasChildren()) return null;
+                  let payload = {
+                    notification: {
+                        title:'Nova Ocorrência',
+                        body:'Uma nova Ocorrência foi registrada'
+                    }
+                }
+                const tokens = Object.keys(token.val());
+                return admin.messaging().sendToDevice(tokens,payload);
+              }
+          )
+    })
